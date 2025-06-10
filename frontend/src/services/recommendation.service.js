eebcdb8cdc95363b9eae5174161d08a376b23eeb
Ronaldo <ronaldocutrim@gmail.com>
2025-06-10 15:10:34 -0300

    fix: ajustes services

--- frontend/src/services/recommendation.service.js
+++ frontend/src/services/recommendation.service.js
@@ -1,19 +1,29 @@
 // getRecommendations.js
 
 const getRecommendations = (
-  formData = { selectedPreferences: [], selectedFeatures: [] },
+  formData = { selectedPreferences: [], selectedFeatures: [], selectedRecommendationType: '' },
   products
 ) => {
-  return products.map((product) => {
+  const matchingProducts = products.map((product) => {
     const { selectedPreferences, selectedFeatures } = formData;
     const { preferences, features } = product;
-    const preferencesMatch = preferences.filter((preference) => selectedPreferences.includes(preference)).length;
-    const featuresMatch = features.filter((feature) => selectedFeatures.includes(feature)).length;
+    const preferencesMatch = preferences.filter((preference) => selectedPreferences?.includes(preference)).length;
+    const featuresMatch = features.filter((feature) => selectedFeatures?.includes(feature)).length;
     return {
       score: preferencesMatch + featuresMatch,
       ...product
     }
   }).filter((product) => product.score > 0);
+
+  const sortedProducts = [...matchingProducts].sort((a, b) => b.score - a.score);
+
+  if (formData.selectedRecommendationType === 'SingleProduct' && sortedProducts.length > 0) {
+    const highestScore = sortedProducts[0].score;
+    const productsWithHighestScore = sortedProducts.filter(product => product.score === highestScore);
+    return [productsWithHighestScore[productsWithHighestScore.length - 1]];
+  }
+
+  return sortedProducts;
 }
 
 export default { getRecommendations };
